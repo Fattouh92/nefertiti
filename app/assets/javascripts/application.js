4b0fe9a486ef68a7a16ee8684f824ed83026f174
Mohamed Aboul Azm <mohamedtamer92@gmail.com>
2013-12-21 20:29:56 +0200

    fixing jquery error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery_ujs
 //= require jquery
+//= require jquery_ujs
+//= require jquery.ui.all
 
 /* ===================================================
  * bootstrap-transition.js v2.3.2
